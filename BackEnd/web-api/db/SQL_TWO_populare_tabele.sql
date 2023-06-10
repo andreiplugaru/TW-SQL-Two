d6d2e749c5928d15ce09eb02eaa81d2313820404
Pal Alexandra <palalexandra3@yahoo.com>
2023-06-10 14:07:47 +0300

    creare 2 tabele noi. Populare baza de date conform schimbarilor

--- BackEnd/web-api/db/SQL_TWO_populare_tabele.sql
+++ BackEnd/web-api/db/SQL_TWO_populare_tabele.sql
@@ -35,20 +35,28 @@ BEGIN
 END;
 
 /
---INSERARE PROBLEMS_CATEGORY
+--INSERARE PROBLEMS_DIFFICULTIES
 BEGIN
-    INSERT INTO problem_categories VALUES (1, 'USOARA');
-    INSERT INTO problem_categories VALUES (2, 'MEDIE');
-    INSERT INTO problem_categories VALUES (3, 'GREA');
+    INSERT INTO problem_difficulties VALUES (1, 'USOARA');
+    INSERT INTO problem_difficulties VALUES (2, 'MEDIE');
+    INSERT INTO problem_difficulties VALUES (3, 'GREA');
 END;
+/
 
+--INSERARE PROBLEM_CATEGORIES
+BEGIN
+    INSERT INTO problem_categories VALUES (1, 'JOIN');
+    INSERT INTO problem_categories VALUES (2, 'GROUP BY');
+    INSERT INTO problem_categories VALUES (3, 'ORDER BY');
+END;
 /
 
 --INSERARE PROBLEMS
 DECLARE 
     v_cerinta problems.requirement%type;
-    v_solutie problems.solution%type;    
+    v_solutie problems.solution%type;
     v_id_category problems.id_category%type;
+    v_id_difficulty problems.id_difficulty%type;
     v_contor INTEGER  :=0;
 BEGIN
     
@@ -56,16 +64,20 @@ BEGIN
         v_cerinta := 'cerinta' || v_contor;
         v_solutie := 'solutie' || v_contor;
         
+        --CATEGORIA CARE ESTE CEVA HARDCODAT DEOCAMDATA
         if v_contor MOD 3 = 0 then
             v_id_category := 1;
         elsif v_contor MOD 3 = 1 then
             v_id_category := 2;
         else  v_id_category := 3;
         end if;
+
+        --BY DEFAULT O PROBLEMA ESTE CONSIDERATA USOARA, ULTERIOR I SE VA MODIFICA DIFICULTATEA LUAND IN CONSIDERARE MARKED_PROBLEMS
+        v_id_difficulty := 1;
         
          --dbms_output.put_line(v_cerinta || ' ' || v_soluie);
         
-        INSERT INTO problems VALUES (v_contor, v_cerinta, v_solutie, v_id_category);
+        INSERT INTO problems VALUES (v_contor, v_cerinta, v_solutie,v_id_category, v_id_difficulty);
        
     end loop;
 
@@ -147,10 +159,63 @@ SELECT * FROM solved_problems;
 --STUD3 (id=5)- ar trebui sa primeasca urm pb si sa fie MEDIE 
     --(in data de 25 mai 2023--acum testez alg, stud cu id=5 are rezolvate in ultimele 3 zile 10 probleme, iar ultima pb rezolvata este din cat MEDIE, iar in total are rez 10 pb din cat MEDIE =>urm pb medie)
 
+--INSERT INTO MARKED_PROBLEMS
+INSERT INTO marked_problems VALUES (3,3, 2);
+INSERT INTO marked_problems VALUES (3,6, 2);
+INSERT INTO marked_problems VALUES (3,9, 2);
+INSERT INTO marked_problems VALUES (3,12,2);
+INSERT INTO marked_problems VALUES (3,15,2);
+INSERT INTO marked_problems VALUES (3,18,2);
+INSERT INTO marked_problems VALUES (3,21,2);
+INSERT INTO marked_problems VALUES (3,24,3);
+INSERT INTO marked_problems VALUES (3,27,3);
+INSERT INTO marked_problems VALUES (3,30, 3);
+INSERT INTO marked_problems VALUES (3,33, 3);
+INSERT INTO marked_problems VALUES (3,36, 3);
+INSERT INTO marked_problems VALUES (3,39, 3);
+INSERT INTO marked_problems VALUES (3,42, 3);
+INSERT INTO marked_problems VALUES (3,45, 3);
+INSERT INTO marked_problems VALUES (3,1, 1);
+INSERT INTO marked_problems VALUES (3,4, 1);
+INSERT INTO marked_problems VALUES (3,7, 1);
+INSERT INTO marked_problems VALUES (3,10, 1);
+INSERT INTO marked_problems VALUES (3,13, 1);
+
+
+INSERT INTO marked_problems VALUES (4,3, 3);
+INSERT INTO marked_problems VALUES (4,6, 3);
+INSERT INTO marked_problems VALUES (4,9, 3);
+INSERT INTO marked_problems VALUES (4,12,3);
+INSERT INTO marked_problems VALUES (4,15,3);
+INSERT INTO marked_problems VALUES (4,18,3);
+INSERT INTO marked_problems VALUES (4,21,3);
+INSERT INTO marked_problems VALUES (4,24,1);
+INSERT INTO marked_problems VALUES (4,27,1);
+INSERT INTO marked_problems VALUES (4,30, 1);
+INSERT INTO marked_problems VALUES (4,33, 1);
+INSERT INTO marked_problems VALUES (4,36, 1);
+INSERT INTO marked_problems VALUES (4,39, 1);
+INSERT INTO marked_problems VALUES (4,42, 1);
+INSERT INTO marked_problems VALUES (4,45, 1);
+INSERT INTO marked_problems VALUES (4,1, 2);
+INSERT INTO marked_problems VALUES (4,4, 2);
+INSERT INTO marked_problems VALUES (4,7, 2);
+INSERT INTO marked_problems VALUES (4,10, 2);
+
+INSERT INTO marked_problems VALUES (5,1,1);
+INSERT INTO marked_problems VALUES (5,4,1);
+INSERT INTO marked_problems VALUES (5,7, 1);
+INSERT INTO marked_problems VALUES (5,10, 1);
+INSERT INTO marked_problems VALUES (5,13, 2);
+INSERT INTO marked_problems VALUES (5,16, 2);
+INSERT INTO marked_problems VALUES (5,19, 2);
+INSERT INTO marked_problems VALUES (5,22, 3);
+INSERT INTO marked_problems VALUES (5,25, 3);
+INSERT INTO marked_problems VALUES (5,29, 3);
+
 
 --INSERT INTO ATTEMPTS:
 INSERT INTO attempts VALUES (1,3,4);
-
 SELECT * FROM attempts;
 
 --INSERT INTO COMMENTS:
