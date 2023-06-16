7a61e1865934749292d70fdee7d7422f98832e66
Pal Alexandra <palalexandra3@yahoo.com>
2023-06-16 10:56:41 +0300

    modelare tabele admin + modificari tabel statistici elev

--- Partea_intai/Interfata/js/administrare.js
+++ Partea_intai/Interfata/js/administrare.js
@@ -1,4 +1,92 @@
-console.log(localStorage.getItem('jwt'));
-if (localStorage.getItem('jwt') === null) {
-    window.open("login.html", "_self");
+import { ADMIN_ACCOUNTS_ENDPOINT, ADMIN_REMOVE_ACCOUNTS_ENDPOINT } from "./endpoints.js";
+import { sendJwtFetchRequest, sendJwtFetchRequestWithoutBody } from "./request/request_handler.js"
+
+
+
+function guard(){
+    if (localStorage.getItem('jwt') === null || localStorage.getItem('role') !== 'ADMIN') {
+        window.open("login.html", "_self");
+    }
 }
+
+const table = document.getElementById('accounts-table');
+const tableBody = document.getElementById('accounts-body');
+table.addEventListener('click', onDeleteRow);
+
+async function getAccounts(){
+    let info;
+    await sendJwtFetchRequestWithoutBody(ADMIN_ACCOUNTS_ENDPOINT, 'GET', localStorage.getItem('jwt'))
+        .then(response => response.json())
+        .then(data => { info = data });
+    displayAccounts(info);
+}
+
+
+
+function displayAccounts(data){
+    //primesc {idStud, userNameStud, emailStud}
+    //ATENTIE LA INTEGRARE
+    for(var i = 0; i < data.length; i++){
+
+        const row = document.createElement('tr');
+        
+        const userCell = document.createElement('td');
+        userCell.innerHTML = data[i].username;
+        userCell.dataset.label='Utilizator';
+
+        const emailCell = document.createElement('td');
+        emailCell.innerHTML = data[i].email;
+        emailCell.dataset.label = 'E-mail';
+
+        const buttonCell = document.createElement('td');
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = 'STERGE';
+        deleteButton.classList.add('btn');
+        deleteButton.style.border = 'none';
+        deleteButton.style.borderRadius = '8px';
+        deleteButton.style.cursor = 'pointer';
+        deleteButton.dataset.userId = data[i].idStud; //pt ca am asta aici
+        buttonCell.appendChild(deleteButton);
+        buttonCell.dataset.label = 'Actiune'
+
+        row.appendChild(idCell);
+        row.appendChild(userCell);
+        row.appendChild(emailCell);
+        row.appendChild(buttonCell);
+        tableBody.appendChild(row);
+
+    }
+}
+
+async function onDeleteRow(e){
+
+    e.preventDefault();
+    if(!e.target.classList.contains('btn')){
+        return;
+    }
+
+    const deleteBtn = e.target;
+    const studentId = deleteBtn.dataset.userId;
+    
+    const request = await sendJwtFetchRequestWithoutBody(ADMIN_REMOVE_ACCOUNTS_ENDPOINT + "?studentId=" + studentId, 'DELETE', localStorage.getItem('jwt'))
+    const status = request.status;
+    if(status === 200){
+        resetTable();
+        getAccounts();
+    }else{
+        console.log("cererea de stergere cont nu a functionat");
+    }
+    
+}
+
+
+function resetTable(){
+    while(tableBody.firstChild){
+        tableBody.removeChild(tableBody.firstChild);
+    }
+}
+
+
+
+guard();
+await getAccounts();
